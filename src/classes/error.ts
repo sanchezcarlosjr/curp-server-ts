2d586d57774818ac599b2186859f1822d1b6b8bd
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-12 11:12:31 -0500

    Update error.ts

--- src/classes/error.ts
+++ src/classes/error.ts
@@ -2,7 +2,7 @@ export class _error {
   'by': string;
   'problem': string;
   'solution': string;
-  constructor(by: "user" | "server", problem: string, solution: string) {
+  constructor(by: string, problem: string, solution: string) {
     this.by = by;
     this.problem = problem;
     this.solution = solution;

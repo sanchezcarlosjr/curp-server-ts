83438bb4c14b9d09c9839696b54b4545b4ea0d02
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-13 16:45:24 -0500

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,39 +1,21 @@
-import * as requestIp from 'request-ip';
-import * as express from 'express';
+import * as requestIp from "request-ip";
+import * as express from "express";
+
 import * as cors from "cors";
+
+import home from './home';
+import curp from './curp';
+
 const router = express.Router();
 
-router.use(cors({
-    origin: '*'
-}));
+router.use(
+  cors({
+    origin: "*",
+  })
+);
 router.use(requestIp.mw());
 
-router.get('/', (request: express.Request, response: express.Response) =>
-    response
-        .status(201)
-        .redirect("https://carlos-eduardo-sanchez-torres.sanchezcarlosjr.com/curp-renapo-api")
-);
-
-import user from "../middlewares/user";
-import {query} from "express-validator";
-import {ApplicationError} from "../classes/error";
-import * as curp from "../utils/curp";
-import validationResult from "../middlewares/validator";
-import {findMexicanByCURP} from "../controllers/CurpController";
-router.use(user);
-router.get(
-    '/curp',
-    query("curp")
-        .exists()
-        .withMessage(new ApplicationError('user', 'The query parameter --curp-- is missing.', 'Add the query parameter.'))
-        .if(query("curp").exists())
-        .toUpperCase()
-        .custom((value) => {
-            return curp.validFormat(value);
-        })
-        .withMessage(new ApplicationError('user', 'The query parameter --curp-- submitted has invalid characters.', 'Correct the curp entered.')),
-    validationResult,
-    findMexicanByCURP);
-
+router.use(home);
+router.use("/curp", curp);
 
 export default router;

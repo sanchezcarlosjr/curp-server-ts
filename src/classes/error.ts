5777fad3268ff49cd7dc5d036aa887f48e27a9ba
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-12 20:19:10 -0500

    Update error.ts

--- src/classes/error.ts
+++ src/classes/error.ts
@@ -8,3 +8,22 @@ export class _error {
     this.solution = solution;
   }
 }
+
+export class processInfo {
+  id: string;
+  file: string;
+  nameProcess: string;
+  position: number;
+  error: string;
+  moreDetails: any;
+  constructor(id: string, file: string, nameProcess: string) {
+    this.id = id;
+    this.file = file;
+    this.nameProcess = nameProcess;
+    this.position = 1;
+    this.error = "err://no-err";
+    this.moreDetails = "err://no-more-details";
+  }
+}
+
+new processInfo("", "", "").error = "asdasdasd"

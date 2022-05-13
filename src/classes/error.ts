1bbea6bc1bd8b15c2df000ae36346f13713ebd0c
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-13 16:44:36 -0500

    Update error.ts

--- src/classes/error.ts
+++ src/classes/error.ts
@@ -1,7 +1,7 @@
 export class ApplicationError {
-  'by': string;
-  'problem': string;
-  'solution': string;
+  "by": string;
+  "problem": string;
+  "solution": string;
   constructor(by: string, problem: string, solution: string) {
     this.by = by;
     this.problem = problem;
@@ -12,18 +12,41 @@ export class ApplicationError {
 export class ProcessInfo {
   id: string;
   file: string;
-  nameProcess: string;
+  type: string;
+  process: string;
   position: number;
   error: string;
-  moreDetails: any;
-  constructor(id: string, file: string, nameProcess: string) {
+  furtherDetails: any;
+  parent: string;
+  constructor(
+    id: string,
+    file: string | string[],
+    type: "Class" | "Function",
+    process: string,
+    parent: string | string[]
+  ) {
     this.id = id;
-    this.file = file;
-    this.nameProcess = nameProcess;
+    if (typeof file === "string") {
+      this.file = file;
+    } else {
+      let filePath = file[1];
+      filePath = filePath.slice(filePath.lastIndexOf("(") + 1, filePath.lastIndexOf(")"));
+      const extension = filePath.search(new RegExp(/[:][\d]+[:][\d]/g));
+      this.file = filePath.slice(0, extension);
+    }
+    this.type = type;
+    let functionName = file[1];
+    functionName = functionName.slice(functionName.indexOf(".") + 1, functionName.indexOf("(") - 1) + "()";
+    this.process = process + ": " + functionName;
     this.position = 1;
     this.error = "err://no-err";
-    this.moreDetails = "err://no-more-details";
+    this.furtherDetails = "err://no-more-details";
+    if (typeof parent === "string") {
+      this.parent = parent;
+    } else {
+      let parentName = parent[1];
+      parentName = parentName.slice(parentName.indexOf(".") + 1, parentName.indexOf("(") - 1) + "()";
+      this.parent = parentName;
+    }
   }
 }
-
-new ProcessInfo("", "", "").error = "asdasdasd"

974b2ddd6b53f2101ac8d6b15881c33c665deacb
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-12 20:17:15 -0500

    Local reporting system

---
+++ src/classes/report.ts
@@ -0,0 +1,35 @@
+import * as fs from "fs";
+
+type data = {
+  id: string;
+  file: string;
+  nameProcess: string;
+  position: number;
+  error: string;
+  moreDetails: any;
+};
+type folders = "system" | "mongoose" | "firebase" | "email";
+type reportResponse = { date: Number; type: folders };
+
+export class Report {
+  static generate(folder: folders, data: data): reportResponse {
+    const date = Date.now();
+    fs.writeFileSync(`src/data/reports/${folder}/date${date}id${data.id}.json`, JSON.stringify(data));
+    return { date, type: folder };
+  }
+
+  private static exist(date: Number, id: string, type: folders) {
+    return fs.existsSync(`src/data/reports/${type}/date${date}id${id}.json`)
+  }
+
+  static get(date: Number, id: string, type: folders): Buffer | null {
+    if (this.exist(date, id, type)) {
+      const data = fs.readFileSync(`src/data/reports/${type}/date${date}id${id}.json`);
+      return data;
+    } else {
+      return null;
+    }
+  }
+
+  // Subir ticket
+}

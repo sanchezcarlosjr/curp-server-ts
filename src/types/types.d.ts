d8a955c5fadf47c761313edc8c771c47977e9396
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-12 20:12:18 -0500

    Update types.d.ts
    New Property:
    -idApi

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -4,12 +4,13 @@ declare global {
   namespace Express {
     interface Request {
       user: {
-        uid: string,
-        email: string,
-        emailVerified: boolean,
-        displayName: string,
-        phoneNumber: string,
+        uid: string;
+        email: string;
+        emailVerified: boolean;
+        displayName: string;
+        phoneNumber: string;
       };
+      idApi: string;
     }
   }
 }

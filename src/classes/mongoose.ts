94a44014a19d463ee3ea83bf4f1df838403a1c12
raulabad98 <81434685+raulabad98@users.noreply.github.com>
2022-05-12 20:19:00 -0500

    Create mongoose.ts

---
+++ src/classes/mongoose.ts
@@ -0,0 +1,67 @@
+import * as express from "express";
+import mongoose from "mongoose";
+
+import { Report } from "./report";
+import { processInfo } from "./error";
+
+function Protect(target: any): void {
+  Object.freeze(target);
+  Object.preventExtensions(target);
+}
+
+type MongoResponse =
+  | { status: "OK"; details: mongoose.Connection }
+  | { status: "ERROR"; details: string, moreDetails: any | string };
+
+let MongoLocalStatus: Boolean = false;
+let currentConnections: { [key: string]: mongoose.Connection } = {};
+
+const _file = "";
+
+@Protect
+export abstract class MongooseManager {
+  static async connection(database: string, req: express.Request): Promise<mongoose.Connection | Boolean> {
+    const info = new processInfo(req.idApi, _file, "MongooseManager.connection()");
+    if (MongoLocal.getStatus()) {
+      const result = await this.getConnection(database);
+      if (result.status === "OK") {
+        return result.details;
+      } else {
+        MongoLocal.updateStatus();
+        info.error = result.details;
+        info.moreDetails = result.moreDetails ?? "err://no-err";
+        Report.generate("mongoose", info);
+        return new Promise((resolve) => resolve(true));
+      }
+    } else {
+      return new Promise((resolve) => resolve(false));
+    }
+  }
+
+  private static async getConnection(dbName: string): Promise<MongoResponse> {
+    try {
+      if (currentConnections[dbName] !== undefined) {
+        return { status: "OK", details: currentConnections[dbName] };
+      } else {
+        const connection = await mongoose
+          .createConnection((process.env.MONGODB_CNN as string) + "?retryWrites=true&w=majority")
+          .asPromise();
+        return { status: "OK", details: connection };
+      }
+    } catch (error: any) {
+      return { status: "ERROR", details: error.message ?? "mongoose: createConnection()", moreDetails: error};
+    }
+  }
+}
+
+@Protect
+class MongoLocal {
+  static getStatus(status = MongoLocalStatus): Boolean {
+    return status;
+  }
+
+  static updateStatus(status = MongoLocalStatus): void {
+    MongoLocalStatus = !status;
+    return;
+  }
+}
